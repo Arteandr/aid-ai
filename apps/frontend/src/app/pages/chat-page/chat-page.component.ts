93f0aa5856fc10bc2a5ee4a0e93188d72348e514
Arteandr <artedamatik@gmail.com>
2025-04-06 01:06:29 +0300

    [frontend] add chats page

--- apps/frontend/src/app/pages/chat-page/chat-page.component.ts
+++ apps/frontend/src/app/pages/chat-page/chat-page.component.ts
@@ -1,9 +1,40 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { TuiButton, TuiError, TuiIcon, TuiTextfield } from '@taiga-ui/core';
+import { TuiDataListWrapper, TuiFieldErrorPipe } from '@taiga-ui/kit';
+import { TuiForm, TuiSearch } from '@taiga-ui/layout';
+import { TuiSelectModule } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'app-chat-page',
   standalone: true,
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.scss',
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    TuiSearch,
+    TuiTextfield,
+    TuiSelectModule,
+    TuiIcon,
+    TuiDataListWrapper,
+    TuiError,
+    TuiFieldErrorPipe,
+    AsyncPipe,
+    TuiForm,
+    TuiButton,
+  ],
 })
-export default class ChatPageComponent {}
+export default class ChatPageComponent {
+  filterForm = new FormGroup({
+    search: new FormControl(''),
+    filter: new FormControl('Все'),
+  });
+  items = ['Все', 'Открытые', 'Закрытые'];
+}
